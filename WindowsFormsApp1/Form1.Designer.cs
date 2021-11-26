4a1b4a1509700a822ac4825d8a840f88bbea3f76
Алексеев Корней < @corp.college.ru>
2021-11-26 14:37:06 +0900

    c# game

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.scoreLabel.Location = new System.Drawing.Point(966, 48);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(40, 42);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "0";
+            this.scoreLabel.Click += new System.EventHandler(this.score_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Coral;
+            this.button3.Location = new System.Drawing.Point(973, 461);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 42);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Выйти из игры";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(972, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 52);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Music Off";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.LightYellow;
+            this.button4.Location = new System.Drawing.Point(972, 266);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 47);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Music On";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Resource1.background;
+            this.ClientSize = new System.Drawing.Size(1097, 524);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.scoreLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap HandlerTexure = Resource1.Handler,
+                    TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+
+
+        }
+
+        private void score_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 8;
+            _targetPosition.Y += _direction.Y * 8;
+
+            if (_targetPosition.X < 0 || _targetPosition.X > 500)
+            {
+                _direction.X *= -1;
+
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < 30)
+            {
+                AddScore(1);
+            }
+            if (distance < 10)
+            {
+                AddScore(5);
+            }
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+            g.DrawImage(HandlerTexure, targetRect);
+            g.DrawImage(TargetTexture, handlerRect);
+
+        }
+        public WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WMP.URL = @"\\192.168.0.245\share student$\Desktop Student$\алексеев корней\Рабочий стол\sound\sound.wav";
+            WMP.controls.play();
+            //WMP.controls.stop();
+            //WMP.close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WMP.URL = @"\\192.168.0.245\share student$\Desktop Student$\алексеев корней\Рабочий стол\sound\sound.wav";
+            //WMP.controls.play();
+            WMP.controls.stop();
+            //WMP.close();        
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Вы действительно хотите выйти?", "Предупреждение", MessageBoxButtons.YesNo);
+            if (res == System.Windows.Forms.DialogResult.Yes)
+            {
+                Form1.ActiveForm.Close();
+
+            }
+            else
+            {
+                return;
+            }
+            Form1 form1 = (Form1)Application.OpenForms["Form1"];
+            if (form1 != null)
+            {
+
+
+
+
+                this.Close();
+            }
+            else
+                MessageBox.Show("Error while changing data in Form1.", "Error");
+
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = _score.ToString();
+        }
+
+
+
+      
+
+
+
+
+
+
+
+}
+}
